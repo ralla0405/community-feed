9c7451ab0d490d80a8b190b1b64117f77bcae30c
Logan <ralla0405@gmail.com>
2024-08-30 16:28:33 +0900

    refact: id, post 누락되어 추가

--- src/main/java/store/kirinit/communityfeed/domain/comment/Comment.java
+++ src/main/java/store/kirinit/communityfeed/domain/comment/Comment.java
@@ -2,22 +2,37 @@ package store.kirinit.communityfeed.domain.comment;
 
 import java.time.LocalDateTime;
 import store.kirinit.communityfeed.domain.like.Like;
+import store.kirinit.communityfeed.domain.post.Post;
 import store.kirinit.communityfeed.domain.post.content.CommentContent;
 import store.kirinit.communityfeed.domain.user.User;
 
 public class Comment {
+    private final Long id;
     private final CommentContent content;
     private final User writer;
+    private final Post post;
     private final Like like;
     private boolean isUpdated;
     private LocalDateTime updatedAt;
 
-    public Comment(String content, User writer) {
-        this.content = new CommentContent(content);
+    public Comment(Long id, Post post, User writer, CommentContent content) {
+        if (post == null) {
+            throw new IllegalArgumentException("게시글이 없습니다.");
+        }
+        if (writer == null) {
+            throw new IllegalArgumentException("작성자가 없습니다.");
+        }
+        this.id = id;
+        this.post = post;
+        this.content = content;
         this.writer = writer;
         this.like = new Like();
         this.isUpdated = false;
         this.updatedAt = LocalDateTime.now();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getContent() {
@@ -30,6 +45,10 @@ public class Comment {
 
     public String getWriterProfileImageUrl() {
         return writer.getProfileImageUrl();
+    }
+
+    public Post getPost() {
+        return post;
     }
 
     public int getLikeCount() {
